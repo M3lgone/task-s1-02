a658aba773236b26f19239d7b66050e36172986d
IsmaelG <ismaelgn89@gmail.com>
2026-03-16 13:51:54 +0100

    exercices 3.b to 5

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+use Dom\Notation;
+
 //Exercici 1
 
 $name = "Isma";
@@ -18,6 +22,8 @@ echo "Precio: " . $price . "<br>";
 echo "Soy joven: " . $soyJoven . "<br>";
 echo "Apellido: " . APELLIDO . "<br>";
 
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
 //Exercici 2
 
 $saludar = "Hello, World!";
@@ -33,6 +39,8 @@ echo strrev($saludar) . "<br>";
 $especializacion = "Aquest es el curs PHP";
 
 echo $especializacion . " " . $saludar . "<br>";
+
+echo "<br>" . "-------------" . "<br>" . "<br>";
 
 //Exercici 3
 
@@ -71,18 +79,89 @@ echo "Suma de totes = " . $X + $Y + $N + $M . "<br>";
 
 echo "Producte de totes = " . $X * $Y * $N * $M . "<br>";
 
+echo "<br>" . "-------------" . "<br>" . "<br>";
+
 //Exercici 3.B
 
 
 
 
+
+function calculate(int $a, int $b, string $operation) {
+
+    switch ($operation) {
+
+        case "suma":
+            return $a + $b;
+            break;
+
+        case "resta":
+            return $a - $b;
+            break;
+
+        case "multiplicacion":
+            return $a * $b;
+            break;
+
+        case "division":
+            if ($b == 0) {
+            return "error";
+            }
+            else {
+            return $a / $b;
+            break;
+            }
+        default:
+            return "Invalid parameter";
+        }
+    }
+
+echo "Resultado 3.B = " . calculate(0, 8, "division"). "<br>";
+
+echo "<br>" . "-------------" . "<br>" . "<br>";
+/*
+   count(int $c) {
+    for ($c = 0; $c <= 10; $c++) {
+        echo "The number is: $c <br>";
+        }
+    }
+
+echo "Resultado 4 = " . count(2). "<br>";
+*/
+
+
+
+function verify (int $nota) {
+
+if ($nota < 33) {
+return "Reprove";
+}
+
+elseif ($nota >= 33 && $nota <= 44) {
+return "Third division";
+}
+
+else if ($nota >= 45 && $nota <= 59) {
+return "Second division";
+}
+
+else {
+return "First division";
+}
+}
+
+
+echo "Resultado 5 = " . verify(32) . "<br>";
+echo "Resultado 5 = " . verify(33) . "<br>";
+echo "Resultado 5 = " . verify(44) . "<br>";
+echo "Resultado 5 = " . verify(45) . "<br>";
+echo "Resultado 5 = " . verify(59) . "<br>";
+echo "Resultado 5 = " . verify(60) . "<br>";
+
 ?>
 
-<!DOCTYPE html>
-<html>
-<body>
 
-echo "<h1>" . APELLIDO . "</h1>";
 
-</body>
-</html>
+
+
+<h2><?= $output?></h2>
